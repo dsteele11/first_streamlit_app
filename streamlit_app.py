88a49e44e43bb0a015296a2ac5309bc62923055c
dsteele11 <85656451+dsteele11@users.noreply.github.com>
2023-09-08 11:31:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,5 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load contains:")
 streamlit.text(my_data_rows)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')"
